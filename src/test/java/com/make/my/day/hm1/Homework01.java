1446d405494ad1129726df68898850a0ac3aabdd
Polina Semenova <polina_semenova@epam.com>
2019-03-12 13:32:36 +0300

    Reverse to Google code style

--- src/test/java/com/make/my/day/hm1/Homework01.java
+++ src/test/java/com/make/my/day/hm1/Homework01.java
@@ -11,119 +11,119 @@ import org.junit.Test;
 
 public class Homework01 {
 
-    @FunctionalInterface
-    private interface Test01 {
+  @FunctionalInterface
+  private interface Test01 {
 
-        /**
-         * Generate whole word from a char array
-         */
-        String createMessage(char[] chars);
+    /**
+     * Generate whole word from a char array
+     */
+    String createMessage(char[] chars);
+  }
+
+  @Test
+  public void concatenateChars() {
+    Test01 sut = chars -> new String(chars);
+
+    String result_1 = sut.createMessage(new char[]{'a', 'b', 'c'});
+    String result_2 = sut.createMessage(new char[]{'H', 'e', 'l', 'l', 'o'});
+    String result_3 = sut.createMessage(new char[]{'T', 'u', 'r', 't', 'l', 'e'});
+
+    assertEquals("abc", result_1);
+    assertEquals("Hello", result_2);
+    assertEquals("Turtle", result_3);
+  }
+
+  @FunctionalInterface
+  private interface Test02 {
+
+    /**
+     * Check reverse word exm: "word" == "drow" -> false exm2: "eye" == "eye"  -> true
+     */
+    boolean isReversedStringTheSame(String word);
+  }
+
+  @Test
+  public void reversedWord() {
+    Test02 sut = word -> word.equals(new StringBuilder(word).reverse().toString());
+
+    boolean result_1 = sut.isReversedStringTheSame("abccba");
+    boolean result_2 = sut.isReversedStringTheSame("level");
+    boolean result_3 = sut.isReversedStringTheSame("cow");
+    boolean result_4 = sut.isReversedStringTheSame("radar");
+    boolean result_5 = sut.isReversedStringTheSame("mellow");
+    boolean result_6 = sut.isReversedStringTheSame("madam");
+
+    assertTrue(result_1);
+    assertTrue(result_2);
+    assertFalse(result_3);
+    assertTrue(result_4);
+    assertFalse(result_5);
+    assertTrue(result_6);
+  }
+
+  @FunctionalInterface
+  private interface Transform {
+
+    /**
+     * Convert String to Integer type
+     */
+    int convert(String input);
+  }
+
+  @FunctionalInterface
+  private interface Summarizer {
+
+    /**
+     * Sum two numbers
+     */
+    int makeSum(int firstNumber, int secondNumber);
+  }
+
+  private class Counter {
+
+    private Transform transform;
+    private Summarizer summarizer;
+
+    public Counter(Transform transform,
+        Summarizer summarizer) {
+      this.transform = transform;
+      this.summarizer = summarizer;
     }
 
-    @Test
-    public void concatenateChars() {
-        Test01 sut = chars -> new String(chars);
-
-        String result_1 = sut.createMessage(new char[]{'a', 'b', 'c'});
-        String result_2 = sut.createMessage(new char[]{'H', 'e', 'l', 'l', 'o'});
-        String result_3 = sut.createMessage(new char[]{'T', 'u', 'r', 't', 'l', 'e'});
-
-        assertEquals("abc", result_1);
-        assertEquals("Hello", result_2);
-        assertEquals("Turtle", result_3);
+    public int getSum(String firstInput, String secondInput) {
+      int firstTransformedNumber = transform.convert(firstInput);
+      int secondTransformedNumber = transform.convert(secondInput);
+      return summarizer.makeSum(firstTransformedNumber, secondTransformedNumber);
     }
+  }
 
-    @FunctionalInterface
-    private interface Test02 {
+  @Test
+  public void transformAndProvideSumWithCounter() {
 
-        /**
-         * Check reverse word exm: "word" == "drow" -> false exm2: "eye" == "eye"  -> true
-         */
-        boolean isReversedStringTheSame(String word);
-    }
+    Transform transform = str -> (str == null) ? 0 : Integer.valueOf(str);
 
-    @Test
-    public void reversedWord() {
-        Test02 sut = word -> word.equals(new StringBuilder(word).reverse().toString());
+    Summarizer increment = (int fn, int sn) -> fn + sn;
 
-        boolean result_1 = sut.isReversedStringTheSame("abccba");
-        boolean result_2 = sut.isReversedStringTheSame("level");
-        boolean result_3 = sut.isReversedStringTheSame("cow");
-        boolean result_4 = sut.isReversedStringTheSame("radar");
-        boolean result_5 = sut.isReversedStringTheSame("mellow");
-        boolean result_6 = sut.isReversedStringTheSame("madam");
+    Counter sut_1 = new Counter(transform, increment);
+    Counter sut_2 = new Counter(transform, increment);
+    Counter sut_3 = new Counter(transform, increment);
 
-        assertTrue(result_1);
-        assertTrue(result_2);
-        assertFalse(result_3);
-        assertTrue(result_4);
-        assertFalse(result_5);
-        assertTrue(result_6);
-    }
+    assertEquals(5, sut_1.getSum("3", "2"));
+    assertEquals(10, sut_2.getSum("5", "5"));
+    assertEquals(57, sut_3.getSum("24", "33"));
+  }
 
-    @FunctionalInterface
-    private interface Transform {
+  @Test
+  public void sortByNameDistinct() {
+    String[] names = {"Fred", "Maggy", "Suzan", "Loid", "Nir", "Lo", "Stefan", "Maximilian"};
 
-        /**
-         * Convert String to Integer type
-         */
-        int convert(String input);
-    }
+    Arrays.sort(names, (fStr, sStr) -> fStr.length() - sStr.length());
 
-    @FunctionalInterface
-    private interface Summarizer {
+    String[] expectedSortedNames = {"Lo", "Nir", "Fred", "Loid", "Maggy",
+        "Suzan", "Stefan", "Maximilian"};
 
-        /**
-         * Sum two numbers
-         */
-        int makeSum(int firstNumber, int secondNumber);
-    }
-
-    private class Counter {
-
-        private Transform transform;
-        private Summarizer summarizer;
-
-        public Counter(Transform transform,
-                       Summarizer summarizer) {
-            this.transform = transform;
-            this.summarizer = summarizer;
-        }
-
-        public int getSum(String firstInput, String secondInput) {
-            int firstTransformedNumber = transform.convert(firstInput);
-            int secondTransformedNumber = transform.convert(secondInput);
-            return summarizer.makeSum(firstTransformedNumber, secondTransformedNumber);
-        }
-    }
-
-    @Test
-    public void transformAndProvideSumWithCounter() {
-
-        Transform transform = str -> (str == null) ? 0 : Integer.valueOf(str);
-
-        Summarizer increment = (int fn, int sn) -> fn + sn;
-
-        Counter sut_1 = new Counter(transform, increment);
-        Counter sut_2 = new Counter(transform, increment);
-        Counter sut_3 = new Counter(transform, increment);
-
-        assertEquals(5, sut_1.getSum("3", "2"));
-        assertEquals(10, sut_2.getSum("5", "5"));
-        assertEquals(57, sut_3.getSum("24", "33"));
-    }
-
-    @Test
-    public void sortByNameDistinct() {
-        String[] names = {"Fred", "Maggy", "Suzan", "Loid", "Nir", "Lo", "Stefan", "Maximilian"};
-
-        Arrays.sort(names, (fStr, sStr) -> fStr.length() - sStr.length());
-
-        String[] expectedSortedNames = {"Lo", "Nir", "Fred", "Loid", "Maggy",
-                "Suzan", "Stefan", "Maximilian"};
-
-        assertArrayEquals(expectedSortedNames, names);
-    }
+    assertArrayEquals(expectedSortedNames, names);
+  }
 }
 
 
